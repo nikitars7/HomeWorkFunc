1274d594b58b49327a7cbebf059c783b04d34c57
Nikita <nikita_rs7@ukr.net>
2022-10-28 00:23:26 +0300

    =)

---
+++ func.js
@@ -0,0 +1,24 @@
+'use strict'
+
+let first = prompt('Введите число которое хотите возвести в степень', '');
+
+let second = prompt('Введите степень', '');
+
+function pow(x = first, n = second) {
+   if (x === '') {
+      alert('Вы не ввели значение');
+      return;
+   }
+   else if (n === '') {
+      alert('Вы не ввели значение для возведения в степень ');
+      return;
+   } else if (isNaN(x) || isNaN(n)) {
+      alert('Вводить можно только цифры!');
+   } else {
+      return alert(`Результат : ${x ** n}`);
+   }
+}
+
+pow();
+
+
