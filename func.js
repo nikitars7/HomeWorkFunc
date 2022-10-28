55c64671c389dd1448e6ced15b8a0a9b5a5ddfdb
Nikita <nikita_rs7@ukr.net>
2022-10-28 12:46:44 +0300

    update

--- func.js
+++ func.js
@@ -7,11 +7,9 @@ let second = prompt('Введите степень', '');
 function pow(x = first, n = second) {
    if (x === '') {
       alert('Вы не ввели значение');
-      return;
    }
    else if (n === '') {
       alert('Вы не ввели значение для возведения в степень ');
-      return;
    } else if (isNaN(x) || isNaN(n)) {
       alert('Вводить можно только цифры!');
    } else {
